0ae8c93cc247d2a392fed758563f9ee557c23980
ZTRdiamond <rikosimon9@gmail.com>
2023-09-18 16:45:19 +0700

    Just update

--- routes/endpoints/anime/tes/tes.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: "jt",
-    path: "/api/tess",
-    type: "get",
-    code: async(req, res, { send }) => {
-        send({message: "hello!"})
-    }
-}
--- routes/util-pages/status.js
+++ routes/util-pages/status.js
@@ -3,7 +3,7 @@ module.exports = {
     path: "/status",
     type: "get",
     code: async (req, res, { userName }) => {
-        res.status(404).json({ status: 404, error: "in progress!"});
+        res.status(404).json({ status: 404, error: "in progress lol"});
         return;
     }
 }
